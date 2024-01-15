08d5da32e068ecf3954a8fe06dd2f236b7c4326b
Avile Sindelo <avilesindelo@gmail.com>
2024-01-15 06:06:05 +0200

    first commit

---
+++ index.js
@@ -0,0 +1,32 @@
+import express from 'express';
+import { engine } from 'express-handlebars';
+import bodyParser from 'body-parser';
+import pgPromise from 'pg-promise';
+
+//Database connection
+//db string
+const connectionString = process.env.DATABASE_URL || '';
+const postgresP = pgPromise();
+
+const app = express();
+//View engine configuration
+app.engine('handlebars', engine());
+app.set('view engine', 'handlebars');
+app.set('views', 'views');
+
+//Middleware config
+app.use(express.static('public'));
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+
+//Routes
+app.get('/', async function(){
+    //Get the database details 
+    //Render a view
+});
+
+let PORT = process.env.PORT || 8080;
+app.listen(PORT, function(){
+    console.log(`Server started at port ${PORT}`);
+});
