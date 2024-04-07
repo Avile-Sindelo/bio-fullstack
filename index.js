c68dfff9ede4ee465c65cd9ebabea8979ce0bcb4
Avile Sindelo <avilesindelo@gmail.com>
2024-04-07 15:45:22 +0200

    98% completed

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ import bodyParser from 'body-parser';
 import pgPromise from 'pg-promise';
 import Database from './database.js';
 import Typed from 'typed.js';
+import emailjs from '@emailjs/nodejs'
 
 const app = express();
 
@@ -30,12 +31,59 @@ app.use(bodyParser.json());
 //Routes
 app.get('/', async function(req, res){
     //Get the database details 
-    console.log('All the Skills of the developer are as follows: ', await database.getAllSkills());
+    // console.log('All the Skills of the developer are as follows: ', await database.getAllSkills());
     const myProjects = await database.getAllProjects();
     const typedText = await database.getTypedText();
     //Render a view
     res.render('index', {skills: await database.getAllSkills(), projects: myProjects.reverse(), typed: typedText});
 });
+
+app.post('/email', async function(req, res){
+    console.log('Gama: ', req.body.customername);
+    console.log('Email: ', req.body.customeremail);
+    console.log('Myalezo: ', req.body.customermessage);
+
+    const serviceID = "service_0uoqn1h";
+    const templateID = "template_cfqu6aa";
+    const data = {
+        name: req.body.customername,
+        email: req.body.customeremail,
+        message: req.body.customermessage
+    };
+
+    emailjs.init({
+        publicKey: 'ZcSgXgprLloKsX4rN'
+      });
+      
+      errorFree(data)? emailjs.send(serviceID, templateID, data)
+      .then(
+        (response) => {
+          console.log('SUCCESS!', response.status, response.text);
+        },
+        (err) => {
+          console.log('FAILED...', err);
+        },
+      ) : console.log('Error occurred!');
+
+      //check for errors
+      function errorFree(data){
+        if(data.name == ''){
+            console.log('Please enter your name');
+            return false;
+        } else if(data.email == ''){
+            console.log('Please don\'t forget to enter your email address');
+            return false;
+        } else if(data.message == ''){
+            console.log('Please make sure you include a message for the developer');
+            return false;
+        } else {
+            console.log('All the fields have been filled well, thank you!')
+            return true;
+        }
+    }
+
+    res.redirect('/');
+})
 
 let PORT = process.env.PORT || 8080;
 app.listen(PORT, function(){
